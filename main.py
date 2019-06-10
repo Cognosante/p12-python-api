1c8b0115afcce3b3b2d5812ddd2c4e267b985c24
Ciprian Sabolovits <ciprian.sabolovits@cognosante.com>
2019-06-10 14:25:32 -0400

    UPDATED CODE

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import json
 from flask import Flask, jsonify
 import boto3
 from botocore.exceptions import ClientError
-
 app = Flask(__name__)
 
 
@@ -19,21 +18,20 @@ def roi():
         return jsonify(data)
 
 
-@app.route('/file/<filename>', methods=['POST'])
-def get_signed_url(filename):
+@app.route('/image/<name>', methods=['POST'])
+def get_signed_url(name):
     s3_client = boto3.client('s3')
     try:
-        response = s3_client.generate_presigned_url('get_object',
-                                                    Params={
-                                                        "Bucket": "cgs-p12",
-                                                        "Key": filename
-                                                    },
-                                                    ExpiresIn=3600)
+        url = s3_client.generate_presigned_url('get_object',
+                                               Params={
+                                                   "Bucket": "cgs-p12",
+                                                   "Key": name
+                                               },
+                                               ExpiresIn=3600)
     except ClientError as e:
         logging.error(e)
         return None
-    # The response contains the presigned URL
-    return jsonify({"url": response})
+    return jsonify({"url": url})
 
 
 if __name__ == '__main__':
